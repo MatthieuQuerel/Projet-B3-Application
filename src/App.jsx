481c0f1685b9db0c61333451c1454e41cede1f85
Querel matthieu <querelmatthieu@gmail.com>
2023-09-17 18:29:34 +0200

    permier push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/Nav_Bar"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div>
+    <h1> test</h1>
+    <NavBar />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav_Bar.jsx
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+
+class NavBar extends Component {
+render(){
+    return (
+        <div>
+            <p>test 2</p>
+        </div>
+    )
+}
+}
+export default NavBar;
